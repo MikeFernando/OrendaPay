2f83edf06b98aac028727e050db44165387785e8
mikera <mike.silva@pixter.com.br>
2023-10-31 13:26:17 -0300

    add validation signin form

---
+++ src/signIn/script.js
@@ -0,0 +1,86 @@
+const form = document.querySelector('#form')
+const email = document.querySelector('#email')
+const errorEmail = document.querySelector('#errorEmail')
+const password = document.querySelector('#password')
+const errorPassword = document.querySelector('#errorPassword')
+const emailRegex =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    validateEmail()
+    validatePassword()
+
+   if (email.value.length === 0 || password.value.length === 0)
+   {
+        errorEmail.innerText = 'Informe um email'
+        errorPassword.innerText = 'Informe a senha'
+        return
+   }
+   else
+   {
+    window.open('https://amazing-griffin-961e34.netlify.app/home')
+   }
+
+})
+
+let time = null
+
+function setError(input, error) {
+    input.parentElement.style.border = '1px solid #dc2626'
+    error.style.display = 'block'
+}
+
+function removeError(input, error) {
+    input.parentElement.style.border = ''
+    error.style.display = 'none'
+}
+
+function validateEmail() {
+    if (email.value.length > 0)
+    {
+        email.addEventListener('keyup', () => {
+            clearTimeout(time)
+
+            time = setTimeout(() => {
+                if (!emailRegex.test(email.value))
+                {
+                    setError(email, errorEmail)
+                }
+                else
+                {
+                    removeError(email, errorEmail)
+                }
+            }, 1500)
+        })
+    }
+    else
+    {
+        setError(email, errorEmail)
+    }
+}
+
+function validatePassword() {
+    if (email.value.length > 0)
+    {
+        password.addEventListener('keyup', () => {
+            clearTimeout(time)
+
+            time = setTimeout(() => {
+                if (password.value.length < 8)
+                {
+                    setError(password, errorPassword)
+                }
+                else
+                {
+                    removeError(password, errorPassword)
+                }
+            }, 1000)
+        })
+    }
+    else
+    {
+        setError(password, errorPassword)
+    }
+
+}
